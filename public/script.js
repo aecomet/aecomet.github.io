38453b3297c1d75e2ff6d8e2cfef219c2552b8af
Tatsuya Oyanagi <sb.gm.0823@gmail.com>
2017-05-08 18:45:01 +0900

    プロフィールページ完成(9割)

--- public/script.js
+++ public/script.js
@@ -1,2 +1,3 @@
 
-$(function(){var pageTopBtn=$('#page_top');$(window).scroll(function(){($(this).scrollTop()>150)?pageTopBtn.fadeIn():pageTopBtn.fadeOut();});pageTopBtn.click(function(){$("html,body").animate({scrollTop:0},'slow');});});
+$(function(){var pageTopBtn=$('#page-top');var height=100;if($(this).scrollTop()<height){pageTopBtn.hide();}
+$(window).scroll(function(){($(this).scrollTop()>height)?pageTopBtn.fadeIn():pageTopBtn.fadeOut();});pageTopBtn.click(function(){$("html,body").animate({scrollTop:0},'slow');});$(".button-collapse").sideNav();(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.9&appId=1158144580925371";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));});
