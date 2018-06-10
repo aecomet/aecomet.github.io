8e9a286398b15da9d30566d214de83d5b1f9f92f
Tatsuya Oyanagi <sb.gm.0823@gmail.com>
2018-06-10 16:52:27 +0900

    Release

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Profile from './Profile'
-import Skill from './Skill'
-import Resume from './Resume'
-import Link from './Link'
-import NotFound from './NotFound'
-
-export { Profile, Resume, Skill, Link, NotFound }
