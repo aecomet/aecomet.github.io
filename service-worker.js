6173f034c96c04d60961bc80c3cd9ab6195e6143
Tatsuya Oyanagi <sb.gm.0823@gmail.com>
2018-06-14 14:00:40 +0900

    Modified service worker.

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,15 @@
  **/
 const CACHE_NAME = 'portfolio-sw-cache' // cache name
 let urlsToCache = [
-    'index.html',
-    'app.js',
-    'app.css',
-    'vendor.js',
-    'vendor.css',
-    'data.js',
-    'component.js',
-    'component.css',
+    '/',
+    '/index.html',
+    '/app.js',
+    '/app.css',
+    '/vendor.js',
+    '/vendor.css',
+    '/data.js',
+    '/component.js',
+    '/component.css',
 ]
 
 // install module
@@ -27,20 +28,31 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
     e.respondWith(
         caches.match(e.request).then(res => {
-            // Checking a response whether it corrects.
-            if (!res || res.status !== 200 || res.type !== 'basic') {
+
+            // If a response has the cache, it returns the data to client.
+            if (res) {
                 return res
             }
 
             // network request
             // TODO: you must use `clone`, because a request handles once only. So, you prepare two files.
-            const responseToCache = res.clone()
+            const fetchRequest = e.request.clone()
 
-            caches.open(CACHE_NAME).then(cache => {
-                cache.put(e.request, responseToCache) // Registering cache data
+            return fetch(fetchRequest).then(response => {
+                // Checking a response whether it corrects.
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response
+                }
+
+                // TODO: you must use `clone`, because a request handles once only. So, you prepare two files.
+                const responseToCache = response.clone()
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(e.request, responseToCache) // Registering cache data
+                })
+
+                return response
             })
-
-            return res
         })
     )
 })
