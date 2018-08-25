03d925c61b77382bcf756abdfbfb63907439e2ac
Tatsuya Oyanagi <sb.gm.0823@gmail.com>
2018-08-25 12:24:12 +0900

    Update UI.

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,20 +1,21 @@
 <template lang="pug">
     v-layout(row wrap justify-center)#profile
         v-flex(md10 xs12).my-5
-            blockquote.display-1.text-xs-center {{ $t('profile.introduction') }}
+            blockquote.display-1.text-xs-left {{ $t('profile.introduction') }}
         v-flex(md10 xs12).my-3
             v-card(hover)
                 v-layout(row wrap)
                     v-flex(md5 xs12)
                         v-card-title(primary-title): span.headline {{ $t('profile.profileTitle') }}
+                        v-card-text: .text-xs-center.display-4: font-awesome-icon(:icon="['fas', 'user-circle']")
                         v-card-text.pa-3
                             .my-2.headline {{ $t('profile.name') }}
-                            //- .my-4.title {{ $t('profile.status') }}
-                        //- v-card-actions
-                            v-list(style="width: 100%;" light)
-                                v-list-tile(v-for="(u, idx) in $t('profile.univ')" :key="`univ-${idx}`" @click="" :href="u.href" target="_blank" avatar ripple)
-                                    v-list-tile-action: v-icon(color="") school
-                                    v-list-tile-content: v-list-tile-title.orange--text.subheading {{ u.name }}
+                            .my-4.subheading
+                                v-icon cake
+                                | &nbsp;{{ $t('profile.birthday') }}
+                            .my-4.subheading
+                                v-icon work
+                                | &nbsp;{{ $t('profile.status') }}
 
                     v-flex(md7 xs12)
                         v-card-title(primary-title): span.headline {{ $t('profile.hobbyTitle') }}
@@ -34,21 +35,29 @@
                         .text-xs-center.body-1.grey--text(v-for="(artist, idx) in $t('profile.favorite.artist')" :key="`artist-${idx}`") {{ artist }}
 
 
-        v-flex(md4 xs6).my-3
-            v-card(hover)
-                v-card-title(primary-title): span.headline {{ $t('profile.from.title') }}
-                v-card-text: span.subheading {{ $t('profile.from.name') }}
-                gmap-map(:center="$t('profile.from.center')" :zoom="opts.zoom" style="height: 300px; width: 100%;")
-                    gmap-marker(:position="$t('profile.from.center')" :clickable="true" :draggable="true" @click="")
+        v-flex(md7 xs8).my-3
+            v-card(color="transparent" height="300" flat tile)
+                gmap-map(:center="$t(`${placeState}.center`)" :zoom="opts.zoom" style="height: 300px; width: 100%;")
+                    gmap-marker(:position="$t(`${placeState}.center`)" :clickable="true" :draggable="true" @click="")
 
-        v-flex(md1 xs0).my-3
+        v-flex(md3 xs4).my-3
+            v-card(color="transparent" height="300" flat tile)
+                v-card-text.text-xs-center.py-0
+                    v-btn(@click="changePlace" color="orange" outline icon small): v-icon loop
 
-        v-flex(md4 xs6).my-3
-            v-card(hover)
-                v-card-title(primary-title): span.headline {{ $t('profile.stay.title') }}
-                v-card-text: span.subheading {{ $t('profile.stay.name') }}
-                gmap-map(:center="$t('profile.stay.center')" :zoom="opts.zoom" style="height: 300px; width: 100%;")
-                    gmap-marker(:position="$t('profile.stay.center')" :clickable="true" :draggable="true" @click="")
+                v-card-text
+                    span.headline {{ $t(`${placeState}.title`) }}
+
+                    v-divider.my-3
+                    span.subheading.d-block.mb-3 {{ $t(`${placeState}.name`) }}
+                    v-btn(:href="$t(`${placeState}.web_page`)" target="_blank" outline round).orange--text
+                        v-icon open_in_new
+                        | Link
+
+        v-flex(md10 xs12).my-3
+            .title.my-2.text-xs-left {{ $t('link.laboratoryTitle')}}
+            v-card(:href="$t('link.laboratory.href')" target="_blank"  color="transparent" flat tile).pa-3
+                .text-xs-left.body-2.mb-1.orange--text {{ $t('link.laboratory.name') }}
 
 </template>
 
@@ -58,12 +67,36 @@
 
     @Component
     export default class Profile extends Vue {
-        opts = {
+        opts:object = {
             zoom: 17,
             scrollWheel: false,
+        };
+
+        places = [
+            'from',
+            'stay'
+        ];
+
+        place = ''
+        placeIdx = 0;
+
+        created() {
+            this.place = this.places[0]
         }
-        created() {}
         mounted() {}
+
+        changePlace () {
+            if (this.placeIdx === this.places.length - 1) {
+                this.place = this.places[0]
+                this.placeIdx = 0
+            } else {
+                this.place = this.places[++this.placeIdx]
+            }
+        }
+
+        get placeState () {
+            return `profile.${this.place}`
+        }
     }
 </script>
 
