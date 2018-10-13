7c07a42be9dad9a2b966db1da7b4c35b41629afa
Tatsuya Oyanagi <sb.gm.0823@gmail.com>
2018-10-13 13:26:06 +0900

    Modified webpack config.

--- webpack.conf.js
+++ webpack.conf.js
@@ -7,6 +7,7 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const CopywebpackPlugin = require('copy-webpack-plugin');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 function assetsPath(_path) {
     const assetsSubDirectory = 'static'
@@ -22,16 +23,16 @@ let baseWebpack = {
     watch: watch,
     mode: environment,
     entry: {
-        app: path.resolve(__dirname, 'src/main.ts'), // The main javascript file
-        component: path.resolve(__dirname, 'src/components/index.ts'), // The main javascript file
-        data: path.resolve(__dirname, 'src/static/data/index.ts') // The main javascript file
+        'public/app': path.resolve(__dirname, 'src/main.ts'), // The main javascript file
+        'public/component': path.resolve(__dirname, 'src/components/index.ts'), // The main javascript file
+        'public/data': path.resolve(__dirname, 'src/static/data/index.ts') // The main javascript file
     },
     optimization: { // optimization chunks (Referecne: https://qiita.com/soarflat/items/1b5aa7163c087a91877d)
         splitChunks: {
             cacheGroups: {
                 vendor: {
                     test: /node_modules/,
-                    name: 'vendor',
+                    name: 'public/vendor',
                     chunks: 'initial', // initial, async, all
                     enforce: true
                 }
@@ -61,23 +62,57 @@ let baseWebpack = {
             template: path.resolve(__dirname, 'src/404.pug'),
             minify: {
                 html5: true,
-                minifyCSS: true,
+                minifyCSS: false,
                 collapseWhitespace: true,
                 removeComments: true,
                 removeEmptyAttributes: true
             },
-            inject: true,
+            inject: false,
             chunksSortMode: 'dependency',
             excludeChunks: ['app', 'vendor', 'data', 'component']
         }),
         new ExtractTextPlugin('[name].css', {
             allChunks: true // TODO: You divide js files, you must add this code.
         }),
+        new WebpackPwaManifest({
+            filename: "public/manifest.json",
+            name: 'My Progressive Portfolio',
+            short_name: 'T.O. Portfolio',
+            description: 'My portfolio! This is created by PWA.',
+            background_color: '#efefef',
+            theme_color: '#fff',
+            display: 'standalone',
+            orientation: 'portrait',
+            crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            ios: {
+                'apple-mobile-web-app-title': 'T.O. Portfolio',
+                'apple-mobile-web-app-status-bar-style': 'black'
+            },
+            icons: [
+                {
+                    src: path.resolve('src/static/icons/apple-touch-icon.png'),
+                    sizes: [128, 144, 152, 192, 256], // multiple sizes
+                    destination: path.join('public/icons', 'ios'),
+                    ios: true
+                },
+                {
+                    src: path.resolve('src/static/icons/icon.png'),
+                    sizes: [128, 144, 152, 192, 256], // multiple sizes
+                    destination: path.join('public/icons', '')
+                }
+            ]
+        }),
+        new ServiceWorkerWebpackPlugin({
+            entry: path.join(__dirname, 'src/service-worker.js'),
+            filename: 'public/service-worker.js',
+            exclude: [ '**/.*', '**/*.hot-update.*', '**/*.map' ],
+            include: [ '*.js', '*.css', '*.html', 'manifest.json', '*.png', '*.ico' ],
+        }),
     ],
     // Output config
     output: {
         path: '', //  Output directory name
-        filename: '[name].js' // Output filename
+        filename: '[name].js', // Output filename
     },
     module: {
         rules: [
@@ -163,15 +198,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
     baseWebpack.output.path = path.resolve(__dirname, './')
     baseWebpack.plugins = baseWebpack.plugins.concat([
         /* === Copy Static files === */
-        new CopywebpackPlugin([ { toType: 'dir', from: path.join(__dirname, 'src/static/images'), to: path.join(__dirname, 'static/images') } ]),
-        new CopywebpackPlugin([ { toType: 'file', from: path.join(__dirname, 'src/service-worker.js'), to: path.resolve(__dirname, 'service-worker.js') } ]),
-        new CopywebpackPlugin([ { toType: 'file', from: path.join(__dirname, 'src/manifest.json'), to: path.resolve(__dirname, 'manifest.json') } ]),
-        /* === Service Worker === */
-        new ServiceWorkerWebpackPlugin({
-            entry: path.join(__dirname, 'src/service-worker.js'),
-            filename: 'service-worker.js',
-            include: [ '*.js', '*.css', '*.html', 'manifest.json', '**/*.png', '**/*.ico' ],
-        }),
+        new CopywebpackPlugin([ { toType: 'dir', from: path.join(__dirname, 'src/static/images'), to: path.join(__dirname, 'public/static/images') } ]),
     ])
     baseWebpack.optimization.minimizer.push(
         new UglifyJSPlugin({
@@ -190,15 +217,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         /* === Copy Static files === */
-        new CopywebpackPlugin([ { toType: 'dir', from: path.join(__dirname, 'src/static/images'), to: path.join(__dirname, 'dist/static/images') } ]),
-        new CopywebpackPlugin([ { toType: 'file', from: path.join(__dirname, 'src/service-worker.js'), to: path.join(__dirname, 'dist/service-worker.js') } ]),
-        new CopywebpackPlugin([ { toType: 'file', from: path.join(__dirname, 'src/manifest.json'), to: path.resolve(__dirname, 'dist/manifest.json') } ]),
-        new ServiceWorkerWebpackPlugin({
-            entry: path.join(__dirname, 'src/service-worker.js'),
-            filename: 'service-worker.js',
-            exclude: [ '**/.*', '**/*.hot-update.*', '**/*.map' ],
-            include: [ '*.js', '*.css', '*.html', 'manifest.json', '**/*.png', '**/*.ico' ],
-        }),
+        new CopywebpackPlugin([ { toType: 'dir', from: path.join(__dirname, 'src/static/images'), to: path.join(__dirname, 'dist/public/static/images') } ]),
     ])
     baseWebpack['devtool'] = 'inline-source-map'
     // local server config
@@ -209,7 +228,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
         // publicPath: path.join(__dirname, 'dist/'),                     // Temporary path on virtual memory
         progress: false,                         // Show progress on console.
         inline: true,                         // The mode of inline.
-        hot: false,                         // use HMR
+        hot: true,                         // use HMR
         clientLogLevel: 'info',                       // The log level(none, error, warning, info)
         historyApiFallback: true
     }
