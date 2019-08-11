eeb00373f1e418303755aa48592cd9680e5fb527
hiyoko3 <sb.gm.0823@gmail.com>
2019-08-11 16:50:52 +0900

    Enabled vuetify-loade.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,58 +1,13 @@
 import Vue from 'vue'
 import 'vuetify/dist/vuetify.min.css'
-import Vuetify, {
-	VApp,
-	VAppBar,
-	VTabs,
-	VTabsSlider,
-	VTab,
-  VCard,
-	VCardText,
-	VCardActions,
-	VChip,
-	VFooter,
-	VContent,
-	VContainer,
-	VFlex,
-	VLayout,
-	VList,
-	VListItem,
-	VIcon,
-	VSpacer,
-	VBtn,
-	VDivider
-} from 'vuetify/lib'
+import Vuetify from 'vuetify/lib'
 import { Touch } from 'vuetify/lib/directives'
 
 Vue.use(Vuetify, {
-  components: {
-		VApp,
-		VAppBar,
-		VTabs,
-		VTabsSlider,
-		VTab,
-		VCard,
-		VCardText,
-		VCardActions,
-		VChip,
-		VFooter,
-		VContent,
-		VContainer,
-		VFlex,
-		VLayout,
-		VList,
-		VListItem,
-		VIcon,
-		VSpacer,
-		VBtn,
-		VDivider
-  },
-  directives: {
-    Touch,
-  },
-})
-
-Vue.use(Vuetify);
+	directives: {
+		Touch
+	}
+});
 
 export default new Vuetify({
   icons: {
