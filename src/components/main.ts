1f8cefb4beb6b7341e6c90f4d58514f527cc9e3d
hiyoko3 <sb.gm.0823@gmail.com>
2020-02-11 16:18:51 +0900

    Preparation portfolio contents.

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,4 +1,63 @@
+class FooterComponent  {
+	constructor() {
+		const footerYear: HTMLInputElement = document.querySelector('footer span#year') as HTMLInputElement;
+		footerYear.innerHTML = `${new Date().getFullYear()}`;
+	}
+}
+
+interface MainInterface {
+	onItemClick():void;
+}
+
+class MainComponent {
+	protected modal: HTMLInputElement;
+	constructor() {
+		this.modal = document.querySelector('#modal') as HTMLInputElement;
+	}
+}
+
+class SkillComponent extends MainComponent implements MainInterface {
+	private skill: HTMLInputElement;
+	constructor() {
+		super();
+		this.skill = document.querySelector('#skill') as HTMLInputElement;
+		this.skill.addEventListener('click', this.onItemClick);
+	}
+
+ 	onItemClick ():void {
+		console.warn('skill');
+	}
+}
+
+class ProfileComponent extends MainComponent implements MainInterface {
+	private profile: HTMLInputElement;
+	constructor() {
+		super();
+		this.profile = document.querySelector('#profile') as HTMLInputElement;
+		this.profile.addEventListener('click', this.onItemClick);
+	}
+
+ 	onItemClick ():void {
+		console.warn('profile');
+	}
+}
+
+class ContactComponent extends MainComponent implements MainInterface {
+	private contact: HTMLInputElement;
+	constructor() {
+		super();
+		this.contact = document.querySelector('#contact') as HTMLInputElement;
+		this.contact.addEventListener('click', this.onItemClick);
+	}
+
+ 	onItemClick ():void {
+		console.warn('contact');
+	}
+}
+
 document.addEventListener('DOMContentLoaded', e => {
-	const footerYear: HTMLInputElement = document.querySelector('footer span#year') as HTMLInputElement;
-	footerYear.innerHTML = `${new Date().getFullYear()}`;
+	const footer = new FooterComponent();
+	const skill = new SkillComponent();
+	const profile = new ProfileComponent();
+	const contact = new ContactComponent();
 });
--- src/static/data/index.ts
+++ src/static/data/index.ts
@@ -1,8 +1,5 @@
-import BaseJSON from './base.json'
 import ProfileJSON from './Profile.json'
 import SkillJSON from './Skill.json'
-import LinkJSON from './Link.json'
-import WorkJSON from './Work.json'
 import ContactJSON from './Contact.json'
 
-export { BaseJSON, ProfileJSON, SkillJSON, LinkJSON, WorkJSON, ContactJSON }
+export { ProfileJSON, SkillJSON, ContactJSON }
