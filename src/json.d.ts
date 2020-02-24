441a2807a5729b35f74d599c476a43d52568adf1
hiyoko3 <hiyoko3@users.noreply.github.com>
2020-02-24 09:40:13 +0000

    deploy: 3536d17d1db08caa3beb2b40e161c8ec9435a396

--- src/json.d.ts
+++
@@ -1,5 +0,0 @@
-// TODO: Apply type to each json file.
-declare module '*.json' {
-    const value: any
-    export = value
-}
--- src/static/data/index.ts
+++
@@ -1,5 +0,0 @@
-import ProfileJSON from './Profile.json'
-import SkillJSON from './Skill.json'
-import ContactJSON from './Contact.json'
-
-export { ProfileJSON, SkillJSON, ContactJSON }
